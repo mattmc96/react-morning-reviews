4735e067d04a6791641da8e78e9d040651b1d434
Matthew Mccarthy <matthewmc96@gmail.com>
2020-08-19 10:58:13 -0500

    working

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
+import React from 'react'
+import './App.css'
+import List from '.List'
 
+export default function App(){
+
+    return(
+        <div className='App'>
+            <h1 className'movie->The Most Amazing Movie List</h1>
+            <List/>
+        </div>
+    )
+
+}
---
+++ src/List.js
@@ -0,0 +1,28 @@
+import React from 'react' App.css
+
+
+class List extends React.Component{
+    constructor(){
+        super()
+    }
+    
+    this.state = {
+        movies: []
+    }
+}    
+
+
+    render(){
+        return (
+            <div>I am the list component</div>
+        )
+    }
+}
+
+
+
+
+
+
+
+export default List 
